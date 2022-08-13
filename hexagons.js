c721ef6b2ac99eee333987958e3b42c19a716b7e
Ray Thomas <raymond.kyle.thomas.1@gmail.com>
2022-08-13 01:21:56 -0400

    frame (water) but at wrong rotation

--- hexagons.js
+++ hexagons.js
@@ -17,34 +17,125 @@ const sites = [];
   abbr[5] = [...abbr[1]].slice(2, 4);
   abbr[-5] = [...abbr[5]];
   for (let y = -5; y <= 5; y++) {
-    for (const x of abbr[y]) sites.push([x, y * h]);
+    for (const x of abbr[y]) sites.push([x, y]);
   }
 }
 
-const centers = [
-  [-6, -2*h], [-6, 0], [-6, 2*h],
-  [-3, -3*h], [-3, -h], [-3, h], [-3, 3*h],
-  [0, -4*h], [0, -2*h], [0, 0], [0, 2*h], [0, 4*h],
-  [3, -3*h], [3, -h], [3, h], [3, 3*h],
-  [6, -2*h], [6, 0], [6, 2*h],
+const centers = [];
+{
+  const abbr = {};
+  abbr[0] = [-4, -2, 0, 2, 4];
+  abbr[3] = [-3, -1, 1, 3];
+  abbr[-3] = [...abbr[3]];
+  abbr[6] = [...abbr[0]].slice(1, 4);
+  abbr[-6] = [...abbr[6]];
+  for (const x of [-6, -3, 0, 3, 6]) {
+    for (const y of abbr[x]) centers.push([x, y]);
+  }
+}
+
+const hexSites = centers.map(c => {
+  const rel = [
+    [ 2, 0], [ 1,  1], [-1,  1],
+    [-2, 0], [-1, -1], [ 1, -1]
+  ];
+  return rel.map(r => sites.findIndex(
+    s => s[0] === c[0] + r[0] && s[1] === c[1] + r[1]
+  ));
+});
+
+const hexTypes = [
+  'brick', 'brick', 'brick',
+  'wood', 'wood', 'wood', 'wood',
+  'grain', 'grain', 'grain', 'grain',
+  'sheep', 'sheep', 'sheep', 'sheep',
+  'rock', 'rock', 'rock',
+  'desert',
 ];
 
-const relative = [
-  [2, 0], [1, h], [-1, h], [-2, 0], [-1, -h], [1, -h]
+
+// const frameVertices = [
+//   [10, 2], [8, 4],
+//   [2, 6], [-2, 6],
+//   [-8, 4], [-10, 2],
+//   [-10, -2], [-8, -4],
+//   [-2, -6], [2, -6],
+//   [8, -4], [10, -2],
+// ];
+const frameVertices = [
+  [9, 3], [0, 6], [-9, 3], [-9, -3], [0, -6], [9, -3],
 ];
+
+
+// const oldSpaces = [
+//   [6, 12, 18, 19, 13, 7],
+//   [18, 24, 30, 31, 25, 19],
+//   [30, 36, 42, 43, 37, 31],
+//   [2, 7, 13, 14, 8, 3],
+//   [13, 19, 25, 26, 20, 14],
+//   [25, 31, 37, 38, 32, 26],
+//   [37, 43, 48, 49, 44, 38],
+//   [0, 3, 8, 9, 4, 1],
+//   [8, 14, 20, 21, 15, 9],
+//   [20, 26, 32, 33, 27, 21],
+//   [32, 38, 44, 45, 39, 33],
+//   [44, 49, 52, 53, 50, 45],
+//   [4, 9, 15, 16, 10, 5],
+//   [15, 21, 27, 28, 22, 16],
+//   [27, 33, 39, 40, 34, 28],
+//   [39, 45, 50, 51, 46, 40],
+//   [10, 16, 22, 23, 17, 11],
+//   [22, 28, 34, 35, 29, 23],
+//   [34, 40, 46, 47, 41, 35],
+// ];
+//
+// const oldCenters = [
+//   [-6, -2*h], [-6, 0], [-6, 2*h],
+//   [-3, -3*h], [-3, -h], [-3, h], [-3, 3*h],
+//   [0, -4*h], [0, -2*h], [0, 0], [0, 2*h], [0, 4*h],
+//   [3, -3*h], [3, -h], [3, h], [3, 3*h],
+//   [6, -2*h], [6, 0], [6, 2*h],
+// ];
+//
+// const relative = [
+//   [2, 0], [1, h], [-1, h], [-2, 0], [-1, -h], [1, -h]
+// ];
 
 const svg = qs('svg');
 const ns = svg.namespaceURI;
 
-for (const [xCenter, yCenter] of centers) {
+// for (const [xCenter, yCenter] of oldCenters) {
+//   const pg = document.createElementNS(ns, 'polygon');
+//   pg.classList.add('hexagon');
+//   for (const [xRel, yRel] of relative) {
+//     const pt = svg.createSVGPoint();
+//     pt.x = xCenter + xRel;
+//     pt.y = yCenter + yRel;
+//     pg.points.appendItem(pt);
+//   }
+//   svg.append(pg);
+// }
+
+{
   const pg = document.createElementNS(ns, 'polygon');
-  pg.classList.add('hexagon');
-  for (const [xRel, yRel] of relative) {
-    const pt = svg.createSVGPoint();
-    pt.x = xCenter + xRel;
-    pt.y = yCenter + yRel;
-    pg.points.appendItem(pt);
+  pg.classList.add('water');
+  const pointsArr = [];
+  for (const [x, y] of frameVertices) {
+    pointsArr.push(x, y * h);
   }
+  pg.setAttribute('points', pointsArr.join(' '));
+  svg.append(pg);
+}
+
+for (const siteArr of hexSites) {
+  const pg = document.createElementNS(ns, 'polygon');
+  pg.classList.add('hex');
+  pg.classList.add(hexTypes.pop());
+  const pointsArr = [];
+  for (const s of siteArr) {
+    pointsArr.push(sites[s][0], sites[s][1] * h);
+  }
+  pg.setAttribute('points', pointsArr.join(' '));
   svg.append(pg);
 }
 
@@ -73,7 +164,15 @@ for (const [i, [x, y]] of Object.entries(sites)) {
   const t = document.createElementNS(ns, 'text');
   t.classList.add('debug-label');
   t.setAttribute('x', x);
-  t.setAttribute('y', y);
+  t.setAttribute('y', y * h);
+  t.innerHTML = i;
+  svg.append(t);
+}
+for (const [i, [x, y]] of Object.entries(centers)) {
+  const t = document.createElementNS(ns, 'text');
+  t.classList.add('debug-label');
+  t.setAttribute('x', x);
+  t.setAttribute('y', y * h);
   t.innerHTML = i;
   svg.append(t);
 }
